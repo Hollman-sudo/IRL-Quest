3ec9c9b7dbc8c1714282367fcc7fe8891bd9abfe
Holman <hollman@email.com>
2026-05-16 15:33:23 -0500

    Agregue la función para reiniciar el día y penalizaciones

--- src/main.py
+++ src/main.py
@@ -19,7 +19,8 @@ def main():
         print("[4] Eventos aleatorios")
         print("[5] Tienda")
         print("[6] Guardar partida")
-        print("[7] Salir")
+        print("[7] Nuevo día")
+        print("[8] Salir")
         
         # Verificamos si la opción es un número o no
         try:
@@ -65,12 +66,16 @@ def main():
                     print("!Misión creada con exito!")
                 # Si el usuario digita 2,  le mostramos todas las misiones diarias que no han sido completadas
                 elif eleccion == 2:
+                    
                     if not misiones_diarias:
                         print("No tiene misiones pendientes.")
                     else:
                         for i, m in enumerate(misiones_diarias):
-                            print(f"{i} - {m['Nombre:']} | {m['Estadistia:']} + {m['Puntos de estadisticas:']} | {m['Oro:']}")   
-               
+                            if not m["Completado:"]:
+                               print(f"{i} - {m['Nombre:']} | {m['Estadistica:']} + {m['Puntos de Estadistica:']} | {m['Oro:']}")   
+                            else:
+                                print(f"{i} - {m['Nombre:']} | (COMPLETADA HOY)")
+                            
                 elif eleccion == 3:
                     if not misiones_diarias:
                         print("No hay misiones por completar")
@@ -86,11 +91,10 @@ def main():
                             # Le damos las recompensas al personaje
                             # Llamamos a la función de completar misiones para dar la recompensa
                             completar_mision(personaje, mision['Estadistica:'], mision['Puntos de Estadistica:'])
+                            mision['Completado:'] = True
                             # Llamamos a la función para ganar oro
                             ganar_oro(personaje, mision["Oro:"])
                             
-                            # Borramos la misión (De momento hasta que pueda meterle que se reinicie con las fechas)
-                            del misiones_diarias[indiceMision]
                             print("!Misión completada con exito!")
                             
                     except ValueError:
@@ -111,8 +115,23 @@ def main():
         elif opcion == 5:
             print("Tienda - ")
         elif opcion == 6:
-            print("Guardando partida...")  
+            print("Guardando partida...")
+            
+            
         elif opcion == 7:
+            penalizacion = 10
+            noCompletadas = 0
+            for m in misiones_diarias:
+                if not m["Completado:"]:
+                    perder_vida(personaje, penalizacion)
+                    noCompletadas += 1
+            for m in misiones_diarias:
+                m["Completado:"] = False
+            print(f"Nuevo días, has perdido {penalizacion * noCompletadas} de vida por {noCompletadas} misiones no completadas.") 
+            print("!EL DÍA HA SIDO REINICIADO CORRECTAMENTE")   
+                
+                
+        elif opcion == 8:
             print("Ciao")
             ejecutando = False
         else:
